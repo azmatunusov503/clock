734613140b5d23de112e68184f29270ead5f3ac0
Azamat <azamatunusov346@gmail.com>
2024-08-16 10:09:28 +0500

    newClockProject

---
+++ main.js
@@ -0,0 +1,83 @@
+const arrHours = document.querySelector('.arr-hours'),
+	arrMinutes = document.querySelector('.arr-minutes'),
+	arrSeconds = document.querySelector('.arr-seconds'),
+	numHours = document.querySelector('.num-hours'),
+	numMinutes = document.querySelector('.num-minutes'),
+	numSeconds = document.querySelector('.num-seconds'),
+	tabLinks = document.querySelectorAll('.tabPanel-links--a'),
+	tabItems = document.querySelectorAll('.tabPanel-content-item'),
+	stopWatchBtn = document.querySelector('.start'),
+	indicator = document.querySelector('.indicator'),
+	stopWatchMinutes = document.querySelector('.minutes span'),
+	stopWatchSeconds = document.querySelector('.seconds span'),
+	stopWatchMiliseconds = document.querySelector('.miliseconds span')
+	
+
+	tabLinks.forEach(function (link, key) {
+	link.addEventListener('click', function () {
+		tabLinks.forEach(function (linkItem, key) {
+			tabLinks[key].classList.remove('active')
+			tabItems[key].classList.remove('active')
+		})
+		link.classList.add('active')
+		tabItems[key].classList.add('active')
+	})
+})
+
+function clock() {
+	let time = new Date(),
+		hours = time.getHours(),
+		minutes = time.getMinutes(),
+		seconds = time.getSeconds()
+
+	arrSeconds.style = `transform: rotate(${seconds * 6}deg);`
+	arrMinutes.style = `transform: rotate(${minutes * 6 + seconds / 10}deg);`
+	arrHours.style = `transform: rotate(${hours * 30 + minutes / 2}deg);`
+
+	numSeconds.innerHTML = seconds < 10 ? `0${seconds}` : seconds
+	numMinutes.innerHTML = minutes < 10 ? `0${minutes}` : minutes
+	numHours.innerHTML = hours < 10 ? `0${hours}` : hours
+	setTimeout(() => {
+		clock()
+	}, 100)
+}
+clock()
+
+stopWatchBtn.addEventListener('click', function(){
+	if(stopWatchBtn.innerHTML === 'start'){
+		stopWatchBtn.innerHTML = 'stop'
+		indicator.classList.add('indicator-start')
+		stopWatch(stopWatchBtn, 0, 55, 0)
+	}else if(stopWatchBtn.innerHTML === 'stop'){
+		stopWatchBtn.innerHTML = 'clear'
+		indicator.classList.remove('indicator-start')
+		indicator.classList.add('indicator-stop')
+	}else{
+		stopWatchBtn.innerHTML = 'start'
+		indicator.classList.remove('indicator-stop')
+	}
+	
+})
+
+function stopWatch(btn, i, sec, min){
+	if(btn.innerHTML === 'stop'){
+		if(i == 100){
+			i = 0
+			if(sec == 60){
+				min++
+				sec = 0
+				stopWatchMinutes.innerHTML = min < 10 ? `0${min}` : min
+				stopWatchSeconds.innerHTML = sec < 10 ? `0${sec}` : sec
+			}else {
+				stopWatchSeconds.innerHTML = sec < 10 ? `0${sec}` : sec
+				sec++
+			}
+		}else{
+			stopWatchMiliseconds.innerHTML = i
+			i++
+		}
+		setTimeout(()=>{
+			stopWatch(btn, i, sec, min)
+		}, 10.1)
+	}
+}
